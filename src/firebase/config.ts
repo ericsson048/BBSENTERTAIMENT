ad5c0e09461a8375d3bcd72c80b0c0577306da45
Ishaka Ericsson <eericsson048@gmail.com>
2025-10-26 08:07:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3729842236-804cc",
+  "appId": "1:21520849693:web:863fafd4a2df745e4c1ff4",
+  "apiKey": "AIzaSyD9mn96AuxgP-4SxwlLcTJNLQ2Qh_L-ytE",
+  "authDomain": "studio-3729842236-804cc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "21520849693"
+};
